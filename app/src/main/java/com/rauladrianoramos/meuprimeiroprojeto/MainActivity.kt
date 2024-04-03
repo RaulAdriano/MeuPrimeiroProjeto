1ab8015916276ae373e3f760f280d1c022663abc
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-03 08:32:09 -0300

    Aula 2 - primeira funcao

--- app/src/main/java/com/rauladrianoramos/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/rauladrianoramos/meuprimeiroprojeto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rauladrianoramos.meuprimeiroprojeto
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun clique(view : View){
+        println("Foi clicado")
     }
 }
